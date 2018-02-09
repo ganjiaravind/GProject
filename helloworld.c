5b9f0d7604f1478fb2db5b465021c04617177f36
ganjiaravind <ganjiaravind@gmail.com>
2018-02-09 15:37:33 +0530

    Added function call

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 
-
+void function(void);
 int main()
 {
 printf("hello world\n");
-printf("hello world 1\n");  
+printf("hello world 1\n");
+function();
 return 0;
+}
+
+
+void function(void)
+{
+  printf("%s\n",__func__);
 }
